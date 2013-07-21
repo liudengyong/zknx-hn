3962a2e1970d4371c1a9bd28ce483a0e03939649
Dengyong <Dengyong@192.168.56.1>
2013-07-21 08:47:42 +0800

    列表头不能被选中

--- zknx-hn/src/com/zknx/hn/functions/common/ProductListAdapter.java
+++ zknx-hn/src/com/zknx/hn/functions/common/ProductListAdapter.java
@@ -82,6 +82,8 @@ public class ProductListAdapter extends CommonListAdapter {
 
 		LinearLayout h = (LinearLayout)inflater.inflate(R.layout.product_list_item, null);
 		
+		// 头不能被选中
+		h.setFocusable(false);
 		h.setBackgroundResource(R.drawable.tab_button_bg);
 
 		// "名字", "最低价", "最高价", "平均价", "产地价", "单位", "添加"
